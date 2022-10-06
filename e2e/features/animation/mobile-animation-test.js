38f8a0f945a70ab78cc834bb625245a70189a8d2
ryanweiler92 <ryanweiler92@gmail.com>
2022-10-06 08:49:38 -0400

    add tests to see if screen is mobile

--- e2e/features/animation/mobile-animation-test.js
+++ e2e/features/animation/mobile-animation-test.js
@@ -2,14 +2,30 @@ const TIME_LIMIT = 10000;
 
 module.exports = {
   before: (c) => {
+    c.url(`${c.globals.url}'?v=-82.73697802714918,27.137724977419197,-71.17181984959728,52.16591344371096&lg=false&t=2022-01-07-T15%3A27%3A49Z`);
+    c.pause(10000);
     c.setWindowSize(375, 667);
     c.pause(10000);
   },
 
+  //if this fails then the screen is not in mobile view
+  'Check if coords case is not visible to see if screen is in desktop view.': (c) => {
+    c.waitForElementNotVisible('#ol-coords-case', TIME_LIMIT);
+  },
+
+  'Check if timeline axis is visible to see if screen is in desktop view': (c) => {
+    c.waitForElementNotPresent('.timeline-axis-container', TIME_LIMIT);
+  },
+
+  'Check for mobile timeline arrows to see if screen is in mobile view': (c) => {
+    c.waitForElementVisible('.mobile-date-change-arrows-btn', TIME_LIMIT);
+  },
+
+  'Check for mobile animate button to see if screen is in mobile view': (c) => {
+    c.waitForElementVisible('.mobile-animate-button', TIME_LIMIT);
+  },
+
   'Mobile animate button opens widget': (c) => {
-    c.url(`${c.globals.url}'?v=-82.73697802714918,27.137724977419197,-71.17181984959728,52.16591344371096&lg=false&t=2022-01-07-T15%3A27%3A49Z`);
-    c.pause(10000);
-    c.waitForElementPresent('.mobile-animate-button', TIME_LIMIT);
     c.waitForElementPresent('.mobile-date-picker-select-btn', TIME_LIMIT);
     c.useCss().click('.mobile-animate-button');
     c.pause(300);
